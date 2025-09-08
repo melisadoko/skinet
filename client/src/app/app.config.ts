243b2b325ad60a33f13f544241e50f2bf52d7f35
Melisa Doko <dokomelisa92@gmail.com>
2025-09-08 15:46:12 +0200

    interceptors added

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,13 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { errorInterceptor } from './core/interceptors/error-interceptor';
+import { loadingInterceptor } from './core/interceptors/loading-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()   
+    provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor]))   
   ]
 };
