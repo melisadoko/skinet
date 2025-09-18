c919cc8869792f755c3d509278ea57792b1d6823
Melisa Doko <dokomelisa92@gmail.com>
2025-09-19 01:03:44 +0200

    order components

--- client/src/app/features/checkout/checkout.component.ts
+++ client/src/app/features/checkout/checkout.component.ts
@@ -16,7 +16,8 @@ import { CartService } from '../../core/services/cart.service';
 import { CurrencyPipe } from '@angular/common';
 import { CheckoutReviewComponent } from './checkout-review/checkout-review.component';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import { ShippingAddress } from '../../shared/models/order';
+import { OrderToCreate, ShippingAddress } from '../../shared/models/order';
+import { OrderService } from '../../core/services/order.service';
 
 @Component({
   selector: 'app-checkout',
@@ -40,6 +41,7 @@ export class CheckoutComponent implements OnInit, OnDestroy {
   private snackbar = inject(SnacbarService);
   private router = inject(Router);
   private accountService = inject(AccountService);
+  private orderService = inject(OrderService);
   cartService = inject(CartService);
   addressElement?: StripeAddressElement;
   paymentElement?: StripePaymentElement;
@@ -122,12 +124,21 @@ export class CheckoutComponent implements OnInit, OnDestroy {
     try {
       if(this.confirmationToken) {
         const result = await this.stripeService.confirmPayment(this.confirmationToken);
-        if(result.error){
-          throw new Error(result.error.message);
-        } else {
+
+        if(result.paymentIntent?.status === 'succeeded') {
+          const order = await this.createOrderModel();
+          const orderResult = await firstValueFrom(this.orderService.createOrder(order));
+          if(orderResult) {
           this.cartService.deleteCart();
           this.cartService.selectedDelivery.set(null);
           this.router.navigateByUrl('/checkout/success');
+          } else {
+            throw new Error('Order creation failed');
+          }
+        } else if(result.error) {
+          throw new Error(result.error.message);
+        } else {
+          throw new Error('Something went wrong');
         }
       }
     } catch(error: any) {
@@ -138,10 +149,26 @@ export class CheckoutComponent implements OnInit, OnDestroy {
     }
   }
 
-  private async createOrderModel() {
+  private async createOrderModel(): Promise<OrderToCreate> {
     const cart = this.cartService.cart();
-    const shippingAddres = await this.getAddressFromStripeAddress() as ShippingAddress;
+    const shippingAddress = await this.getAddressFromStripeAddress() as ShippingAddress;
     const card = this.confirmationToken?.payment_method_preview.card;
+    
+    if(!cart?.id || !cart.deliveryMethodId || !card || !shippingAddress) {
+      throw new Error('Problem creating order');
+    }
+
+    return {
+      cartId: cart.id,
+      paymentSummary: {
+        last4: +card.last4,
+        brand: card.brand,
+        expMonth: card.exp_month,
+        expYear: card.exp_year
+      },
+      deliveryMethodId: cart.deliveryMethodId,
+      shippingAddress
+    }
   }
 
   private async getAddressFromStripeAddress() : Promise<Address | ShippingAddress | null> {
--- client/src/app/features/orders/order-detailed/order-detailed.component.ts
+++ client/src/app/features/orders/order-detailed/order-detailed.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { OrderService } from '../../../core/services/order.service';
+import { ActivatedRoute } from '@angular/router';
+import { Order } from '../../../shared/models/order';
+import { MatCardModule } from '@angular/material/card';
+import { MatButton } from '@angular/material/button';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-order-detailed',
-  imports: [],
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatButton,
+    DatePipe,
+    CurrencyPipe
+  ],
   templateUrl: './order-detailed.component.html',
   styleUrl: './order-detailed.component.scss'
 })
-export class OrderDetailedComponent {
+export class OrderDetailedComponent implements OnInit {
+    private orderService =  inject(OrderService);
+    private activatedRoute = inject(ActivatedRoute);
+    order?: Order;
 
+    ngOnInit(): void {
+      this.loadOrder();
+    }
+
+    loadOrder() {
+       const id = this.activatedRoute.snapshot.paramMap.get('id');
+      if(!id) return;
+      this.orderService.getOrderByDetailed(+id).subscribe({
+        next: order => this.order = order
+      })
+    }
 }
